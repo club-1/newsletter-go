41949196f69543d6497c2c3c6923cd0e610f947b
vincent-peugnet <vincent-peugnet@riseup.net>
2025-11-11 18:23:10 +0100

    create config folder and files if not existing
    generate a random .secret file if not existing

--- base.go
+++ base.go
@@ -3,7 +3,10 @@ package newsletter
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
 )
@@ -64,6 +67,16 @@ func readLines(path string) ([]string, error) {
 // 	return w.Flush()
 // }
 
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+
+func randStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
 // Load config in newsletter.Conf struct
 func ReadConfig() error {
 	homeDir, err := os.UserHomeDir()
@@ -71,37 +84,75 @@ func ReadConfig() error {
 		return fmt.Errorf("could not get user's home: %w", err)
 	}
 
-	emailsFilePath := filepath.Join(homeDir, ConfigPath, EmailsFile)
-	emails, err := readLines(emailsFilePath)
+	configDir := filepath.Join(homeDir, ConfigPath)
+	err = os.MkdirAll(configDir, 0775)
 	if err != nil {
-		return fmt.Errorf("could not get emails: %w", err)
-	}
-	signatureFilePath := filepath.Join(homeDir, ConfigPath, SignatureFile)
-	signature, err := os.ReadFile(signatureFilePath)
-	if err != nil {
-		return fmt.Errorf("could not get signature: %w", err)
-	}
-	secretFilePath := filepath.Join(homeDir, ConfigPath, SecretFile)
-	secret, err := os.ReadFile(secretFilePath)
-	if err != nil {
-		return fmt.Errorf("could not get secret: %w", err)
+		return fmt.Errorf("could not init config directory: %w", err)
 	}
 
-	settingsFilePath := filepath.Join(homeDir, ConfigPath, SettingsFile)
-	settingsJson, err := os.ReadFile(settingsFilePath)
-	if err != nil {
-		return fmt.Errorf("could not get settings: %w", err)
+	var emails []string
+	emailsFilePath := filepath.Join(homeDir, ConfigPath, EmailsFile)
+	_, err = os.Stat(emailsFilePath)
+	if errors.Is(err, os.ErrNotExist) {
+		emails = []string{}
+	} else {
+		emails, err = readLines(emailsFilePath)
+		if err != nil {
+			return fmt.Errorf("could not get emails: %w", err)
+		}
 	}
+
+	var signature string
+	signatureFilePath := filepath.Join(homeDir, ConfigPath, SignatureFile)
+	_, err = os.Stat(signatureFilePath)
+	if errors.Is(err, os.ErrNotExist) {
+		signature = ""
+	} else {
+		signatureB, err := os.ReadFile(signatureFilePath)
+		if err != nil {
+			return fmt.Errorf("could not get signature: %w", err)
+		}
+		signature = string(signatureB)
+	}
+
+	var secret string
+	secretFilePath := filepath.Join(homeDir, ConfigPath, SecretFile)
+	_, err = os.Stat(secretFilePath)
+	if errors.Is(err, os.ErrNotExist) {
+		secret = randStringRunes(40)
+		err := os.WriteFile(secretFilePath, []byte(secret+"\n"), 0660)
+		if err != nil {
+			return fmt.Errorf("could not store generated secret: %w", err)
+		}
+		log.Print("generated secret")
+	} else {
+		secretB, err := os.ReadFile(secretFilePath)
+		if err != nil {
+			return fmt.Errorf("could not get secret: %w", err)
+		}
+		secret = string(secretB)
+	}
+
 	var settings Settings
-	err = json.Unmarshal(settingsJson, &settings)
-	if err != nil {
-		return fmt.Errorf("could not decode settings: %w", err)
+	settingsFilePath := filepath.Join(homeDir, ConfigPath, SettingsFile)
+	_, err = os.Stat(settingsFilePath)
+	if errors.Is(err, os.ErrNotExist) {
+		settings = Settings{}
+	} else {
+		settingsJson, err := os.ReadFile(settingsFilePath)
+		if err != nil {
+			return fmt.Errorf("could not get settings: %w", err)
+		}
+		err = json.Unmarshal(settingsJson, &settings)
+		if err != nil {
+			return fmt.Errorf("could not decode settings: %w", err)
+		}
 	}
 
 	Conf = &Config{
 		Emails:    emails,
-		Signature: string(signature),
-		Secret:    string(secret),
+		Signature: signature,
+		Secret:    secret,
 		Settings:  settings,
 	}
 	return nil
