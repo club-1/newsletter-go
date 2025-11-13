455b16e692a5a4456e4b75610f6ed7f14f5ea5b9
vincent-peugnet <vincent-peugnet@riseup.net>
2025-11-13 23:52:03 +0100

    add SendEmail() function

--- mail.go
+++ mail.go
@@ -1,5 +1,70 @@
 package newsletter
 
+import (
+	"bytes"
+	"fmt"
+	"mime/quotedprintable"
+	"os/exec"
+	"strings"
+)
+
 type Mail struct {
-	Body string
+	FromAddr  string
+	FromName  string
+	To        string
+	Id        string
+	InReplyTo string
+	ReplyTo   string
+	Subject   string
+	Body      string
+}
+
+func (m *Mail) From() string {
+	return m.FromName + " <" + m.FromAddr + ">"
+}
+
+func quotedPrintable(s string) (string, error) {
+	var ac bytes.Buffer
+	w := quotedprintable.NewWriter(&ac)
+	_, err := w.Write([]byte(s))
+	if err != nil {
+		return "", err
+	}
+	err = w.Close()
+	if err != nil {
+		return "", err
+	}
+	return ac.String(), nil
+}
+
+func SendMail(mail *Mail) error {
+	encodedBody, err := quotedPrintable(mail.Body)
+	if err != nil {
+		return fmt.Errorf("could not encode body: %w", err)
+	}
+
+	args := []string{
+		"-s", mail.Subject,
+		"-r", mail.From(),
+		"-a", "Content-Transfer-Encoding: quoted-printable",
+		"-a", "Content-Type: text/plain; charset=UTF-8",
+		"-a", "Message-Id: " + mail.Id,
+	}
+	if mail.InReplyTo != "" {
+		args = append(args, "-a", "In-Reply-To: "+mail.InReplyTo)
+	}
+	if mail.ReplyTo != "" {
+		args = append(args, "-a", "Reply-To: "+mail.ReplyTo)
+	}
+	args = append(args, "--", mail.To)
+
+	cmd := exec.Command("mailx", args...)
+	cmd.Stdin = strings.NewReader(encodedBody)
+	var out strings.Builder
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("could not execute command: %w %s", err, out.String())
+	}
+	return nil
 }
